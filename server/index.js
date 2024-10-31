3086f667d970b11cf9315542d04ca15256bbad4c
Tatwona McGuire <tmcgu59@wgu.edu>
2024-10-31 17:28:05 -0400

    .

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
+console.log("API Key:", process.env.WEATHER_API_KEY);
 
 const app = express();
 app.use(cors());
