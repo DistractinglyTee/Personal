0b6052751ea9730cc774eba3e62668ad5c3893ba
Tatwona McGuire <tmcgu59@wgu.edu>
2024-10-31 17:21:12 -0400

    .

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY; // API key from .env file
 const PORT = process.env.PORT || 5000;
 
 app.get('/api/weather', async (req, res) => {
@@ -16,11 +16,10 @@ app.get('/api/weather', async (req, res) => {
   }
 
   try {
-    const response = await axios.get(`http://api.weatherstack.com/current
-    ?access_key = 43d196694d09aa6d55ea7ad22469e729`, {
+    const response = await axios.get(`http://api.weatherstack.com/current`, {
       params: {
-        key: WEATHER_API_KEY,
-        q: location,
+        access_key: WEATHER_API_KEY, // use the API key here
+        query: location,             // Weatherstack uses 'query' for location
       },
     });
     res.json(response.data);
